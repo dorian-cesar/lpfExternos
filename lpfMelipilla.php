c94adb1863bc94ebb94e25effd06b6a8de6b8d81
Dorian-Cesar <dorian.celu@gmail.com>
2024-12-24 11:57:27 -0300

    add Sanjose/Melipilla

---
+++ lpfMelipilla.php
@@ -0,0 +1,11 @@
+<?php 
+
+$user="Melipilla";
+$pasw="123";
+$name='Municipalidad de Melipilla';
+
+include __DIR__.'/test.php' ;
+
+lpf($user,$pasw,$name);
+
+?>
---
+++ lpfSJMaipo.php
@@ -0,0 +1,11 @@
+<?php 
+
+$user="SJMaipo";
+$pasw="123";
+$name='Municipalidad de San Jose del Maipo';
+
+include __DIR__.'/test.php' ;
+
+lpf($user,$pasw,$name);
+
+?>
