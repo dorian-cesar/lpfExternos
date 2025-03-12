4d3da5856d866e716037f03229552fd1008d3736
Dorian-Cesar <dorian.celu@gmail.com>
2025-03-12 11:07:57 -0300

    add Rancagua+Buin

---
+++ lpfBuin.php
@@ -0,0 +1,11 @@
+<?php 
+
+$user="Buin";
+$pasw="123";
+$name='Municipalidad de Buin';
+
+include __DIR__.'/test.php' ;
+
+lpf($user,$pasw,$name);
+
+?>
---
+++ lpfRancagua.php
@@ -0,0 +1,11 @@
+<?php 
+
+$user="Rancagua";
+$pasw="123";
+$name='Municipalidad de Rancagua';
+
+include __DIR__.'/test.php' ;
+
+lpf($user,$pasw,$name);
+
+?>
